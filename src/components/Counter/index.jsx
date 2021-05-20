479108ef04b6080f6b18fd6571a590feb90ab01c
JD Wilson <jaydee.wilson@gmail.com>
2021-05-20 16:13:05 +0200

    commit

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -4,13 +4,17 @@ import counteContext from '../../context/counteStore'
 
 
 export default function Counter() {
- let {counterState, dispatch} = useContext(counteContext)
- console.log(counterState)
+    let { counterState, dispatch } = useContext(counteContext)
+    console.log(counterState)
     return (
-        <div className="Conter-card">
-            <button className='plus' onClick={ ()=> dispatch("increment" )  }> + </button>
-            {counterState.counterValue }
-            <button className='minus' onClick={ ()=> dispatch("decrement" ) }> - </button> 
+        <div >
+           <h2>click to add content </h2>
+            <div className="Conter-card">
+                <button className='plus' onClick={() => dispatch("increment")}> + </button>
+                {counterState.counterValue}
+                <button className='minus' onClick={() => dispatch("decrement")}> - </button>
+                {counterState.content.map(text => <p> {text} </p>)}
+            </div>
         </div>
     )
 }
--- src/context/counteStore.jsx
+++ src/context/counteStore.jsx
@@ -7,13 +7,27 @@ let CounterContext = createContext({
 export default CounterContext;
 
 export function CounterContextProvider(props) {
-    let initialState = { counterValue: 0 }
+    let initialState = {
+        counterValue: 0,
+        content: [],
+    }
     let reducer = (state, action) => {
         switch (action) {
             case 'increment':
-                return { counterValue: state.counterValue + 1 }
+            
+                if (state.counterValue >= 0) {
+                    return {
+                        counterValue: state.counterValue + 1,
+                        content: [...state.content,  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, et ea inventore aliquam veniam exercitationem rerum impedit deserunt quaerat magni, similique cumque asperiores sunt dolorum natus soluta quia fuga aperiam atque. Dicta nisi minus enim officiis doloremque, nemo facilis id ab animi facere alias dolores qui magnam accusantium tenetur eum! Incidunt temporibus, pariatur distinctio quo voluptas est perferendis laborum eveniet, praesentium quam eligendi tenetur maxime culpa voluptates ullam, magni fugit doloremque reiciendis eos perspiciatis? Sint ea, tenetur quo distinctio enim blanditiis doloremque repellendus. Rerum laboriosam aut molestiae deleniti fuga iusto deserunt similique ipsam veniam eum! Pariatur iure magnam quaerat quasi. ']
+                    }
+                }
+                    return {counterValue: state.counterValue + 1 , content: [...state.content] }
+               
             case 'decrement':
-                return { counterValue: state.counterValue - 1 };
+                if (state.counterValue >= 0) {
+                    return { counterValue: state.counterValue - 1, content: state.content.filter((_, i) => i !== state.counterValue - 1) };
+                } 
+                return {counterValue: state.counterValue - 1 , content: [] }
             default:
                 throw new Error('Wrong Action')
         }
